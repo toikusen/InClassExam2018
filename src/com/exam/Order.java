7cbbce9da9eff6eb7d07f55aeb7d6a1ce99ba984
toikusen <tuyucheng0407@gmail.com>
2018-12-11 12:05:49 +0800

    unfinish

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,5 +1,22 @@
 package com.exam;
 
-public class Order {
+import java.util.ArrayList;
 
+public class Order {
+	
+	public void add(){
+		ArrayList ticket = new ArrayList();
+		for (int i=0 ;i<1; i++){
+			Ticket t = new Ticket(i);
+			ticket.add(t);
+		}
+		System.out.println(ticket.size());
+	}
+
+	public void print(){
+		for (int i=0 ;i<1; i++){
+			System.out.print("Taipei    Keelung");
+
+		}
+	}
 }
