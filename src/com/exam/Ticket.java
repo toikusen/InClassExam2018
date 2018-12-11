86f502bcdd69dc70b6ffe84862724e4f81c8a441
toikusen <tuyucheng0407@gmail.com>
2018-12-11 12:04:16 +0800

    unfinish

--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -1,5 +1,17 @@
 package com.exam;
 
 public class Ticket {
-
+	
+	String Taipei;
+	String Keelung;
+	int price;
+	int quantity;
+	
+	public Ticket(int price){
+		this.price = price;
+	}
+	
+	public int quantity(){
+		this.quantity = quantity;
+	}
 }
